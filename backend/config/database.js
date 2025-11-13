6a0bb6dc11a6e85a56a03b269810839ee7cbed0d
WebsterFever <webstersevenlanguages@gmail.com>
2025-11-13 14:33:36 -0500

    new fetures

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,24 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// module.exports = sequelize;
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
+
+console.log("Loaded DATABASE_URL:", process.env.DATABASE_URL);
+console.log("Loaded JWT_SECRET:", process.env.JWT_SECRET);
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
