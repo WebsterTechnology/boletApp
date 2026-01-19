7da5b7dc89270da3c1c68dd7956acbd2ce834d58
WebsterFever <webstersevenlanguages@gmail.com>
2026-01-18 21:53:41 -0500

    add withdraw emailjs

--- backend/routes/deChifRoutes.js
+++ backend/routes/deChifRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const deChifController = require("../controllers/dechifControllerrr");
+
+// ✅ MUST MATCH FILE NAME EXACTLY
+const deChifController = require("../controllers/dechifController");
+
 const authenticate = require("../middleware/authenticate");
 
 router.post("/", authenticate, deChifController.createDeChif);
 router.get("/", authenticate, deChifController.getMyDeChifBets);
 router.put("/:id", authenticate, deChifController.updateDeChif);
 router.delete("/:id", authenticate, deChifController.deleteDeChif);
-// Additional route to get all DeChif bets (admin only)
 
 module.exports = router;
