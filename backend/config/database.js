635bf6a50b537a0b967e4dc9f5e22ea3e5773f41
WebsterFever <webstersevenlanguages@gmail.com>
2026-02-10 17:14:14 -0500

    add confif

--- backend/config/database.js
+++ backend/config/database.js
@@ -46,9 +46,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 2,        // 🔥 VERY IMPORTANT for Render
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
+
 
 module.exports = sequelize;
