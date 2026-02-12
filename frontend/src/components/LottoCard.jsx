3c2b735aac2c43ec657ed14f4763b40c2ca9fb92
WebsterFever <webstersevenlanguages@gmail.com>
2026-02-11 23:11:48 -0500

    fix pix  step

--- frontend/src/components/LottoCard.jsx
+++ frontend/src/components/LottoCard.jsx
@@ -1,11 +1,30 @@
-// src/components/LottoCard.jsx
-import React from 'react';
-import styles from '../style/LottoCard.module.css';
-import lotoImg from '../assets/loto.png';
+// // src/components/LottoCard.jsx
+// import React from 'react';
+// import styles from '../style/LottoCard.module.css';
+// import lotoImg from '../assets/loto.png';
+// import htloto from '../assets/htloto.png';
+
+// const LottoCard = ({ openLogin }) => {
+//   return (
+//     <div className={styles.card} onClick={openLogin}>
+//       <img src={lotoImg} alt="Lotto" className={styles.image} />
+//     </div>
+//   );
+// };
+
+// export default LottoCard;
+import React from "react";
+import styles from "../style/LottoCard.module.css";
+import lotoImg from "../assets/loto.png";
+import htloto from "../assets/htloto.png";
 
 const LottoCard = ({ openLogin }) => {
   return (
-    <div className={styles.card} onClick={openLogin}>
+    <div
+      className={styles.card}
+      onClick={openLogin}
+      style={{ backgroundImage: `url(${htloto})` }}
+    >
       <img src={lotoImg} alt="Lotto" className={styles.image} />
     </div>
   );
