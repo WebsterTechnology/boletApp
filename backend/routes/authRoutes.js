558e49b526bb40e158c945fb42c1634a838d4def
WebsterFever <webstersevenlanguages@gmail.com>
2026-03-17 22:53:08 -0400

    add new features delete user

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const authenticate = require("../middleware/authenticate")
+const adminOnly = require("../middleware/adminOnly");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.delete("/users/:id", authenticate, authController.deleteUser);
+router.delete(
+  "/users/:id",
+  authenticate,
+  adminOnly,
+  authController.deleteUser
+);
 
 module.exports = router;
