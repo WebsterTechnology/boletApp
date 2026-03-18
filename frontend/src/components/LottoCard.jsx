c50ca685e9e9c189b1e2f7035f9ec937afe05c75
WebsterFever <webstersevenlanguages@gmail.com>
2026-03-17 20:13:00 -0400

    add new

--- frontend/src/components/LottoCard.jsx
+++ frontend/src/components/LottoCard.jsx
@@ -1,7 +1,7 @@
 // src/components/LottoCard.jsx
 import React from 'react';
 import styles from '../style/LottoCard.module.css';
-import lotoImg from '../assets/loto.png';
+
 import htloto from '../assets/htloto.png';
 
 const LottoCard = ({ openLogin }) => {
